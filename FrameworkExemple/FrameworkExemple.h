1ec1b867c048886fdcac4f6e00bed2ae66cf98c1
macbook <mac>
2017-12-10 15:17:12 +0100

    Initial Commit

---
+++ FrameworkExemple/FrameworkExemple.h
@@ -0,0 +1,19 @@
+//
+//  FrameworkExemple.h
+//  FrameworkExemple
+//
+//  Created by macbook on 10/12/2017.
+//  Copyright © 2017 bessem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FrameworkExemple.
+FOUNDATION_EXPORT double FrameworkExempleVersionNumber;
+
+//! Project version string for FrameworkExemple.
+FOUNDATION_EXPORT const unsigned char FrameworkExempleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkExemple/PublicHeader.h>
+
+
